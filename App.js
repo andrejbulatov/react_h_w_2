14faf271ad0e4c74c36e463e3885ea75ef0fe6c6
Andrei Bulatov <andbulatov@mail.ru>
2024-06-15 17:40:38 +0300

    add task_react_2

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CommentsList from "./components/CommentsList";
+
+function App() {
+  return (
+    <div>
+      <CommentsList />
+    </div>
+  );
+}
+
+export default App;
